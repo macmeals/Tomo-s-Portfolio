170887b4c0436250ee423165690c029264dadb7c
macmeals <loseworld9731@gmail.com>
2022-03-13 23:30:43 +0900

    ハンバーガーメニューを

--- js/hamberger.js
+++ js/hamberger.js
@@ -2,6 +2,7 @@ jQuery( function( $ ) {
 	$( ".p-hamberger" ).on( "click", function() {
 			$( ".c-btn__cross" ).toggleClass( "is-opencross" );
 			$( ".c-hamburger__header" ).toggleClass( "is-open" );
+			$( ".c-hamburger__side" ).toggleClass( "is-open" );
 	} );
 	
 } );
