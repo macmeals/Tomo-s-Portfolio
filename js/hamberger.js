1cd96e0dde7f8b84106dcbcfa8928d5ea3372347
macmeals <loseworld9731@gmail.com>
2022-03-19 13:29:16 +0900

    TopページのiPad対応完了

--- js/hamberger.js
+++ js/hamberger.js
@@ -2,7 +2,8 @@ jQuery( function( $ ) {
 	$( ".p-hamberger" ).on( "click", function() {
 			$( ".c-btn__cross" ).toggleClass( "is-opencross" );
 			$( ".c-hamburger__header" ).toggleClass( "is-open" );
-			$( ".c-hamburger__side" ).toggleClass( "is-open" );
+			$( ".c-hamburger__background" ).toggleClass( "is-open" );
+			$( ".l-side" ).toggleClass( "is-open" );
 	} );
 	
 } );
